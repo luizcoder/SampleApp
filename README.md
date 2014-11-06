3c01319c11e5dc915517e08d3ec34f61b479ae51
Luiz <luiz@stillos.com.br>
2014-11-06 09:03:21 -0200

    Limpeza realizada para inicio de um novo modelo.

--- README.md
+++
@@ -1,3 +0,0 @@
-SampleApp
-=========
-This app was made to be an example and to be used as a sample to start build another app.
--- test/spec.js
+++
@@ -1,8 +0,0 @@
-// spec.js
-var host = 'http://localhost:8100/';
-describe('angularjs homepage', function() {
-  it('should have a title', function() {
-    browser.get(host);
-    expect(browser.getTitle()).toEqual('SampleApp');
-  });
-});
